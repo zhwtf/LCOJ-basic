3ec3d76b880ca82a6685f925ad467e90c2b0c8de
郑昊 <zhenghao@zhenghaodeMacBook-Air.local>
2016-12-29 17:42:14 -0800

    21

---
+++ Linked list/21_Merge Two Sorted Lists.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        //first create a empty pointer used to hold the new combined list
+        ListNode *ptr;
+        if(l1->val >= l2->val){
+            ptr = l2;
+            l2 = l2->next;
+        }
+        else{
+            ptr = l1;
+            l1 = l1->next;
+        }
+        ListNode *dummyhead = ptr;
+        while(l1 != NULL && l2 != NULL){
+            if (l1->val >= l2->val){
+                ptr->next = l2;
+                l2 = l2->next;
+                ptr = ptr->next;
+            }
+            else{
+                ptr->next = l1;
+                l1 = l1->next;
+                ptr = ptr->next;
+            }
+        }
+        //look for which list finished first
+        if (l1 == NULL){
+            //append all the rest of l2 to ptr
+            ptr->next = l2;
+        }
+        else{
+            ptr->next = l1;
+        }
+        return dummyhead;
+    }
+};
+
+
+
+
+//14 line clean C++ Solution
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
