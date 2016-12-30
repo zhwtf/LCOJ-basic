2c26916ad8b43fc9b65788fd2d594b929ebadaec
郑昊 <zhenghao@zhenghaodeMacBook-Air.local>
2016-12-29 17:47:21 -0800

    21-2

--- Linked list/21_Merge Two Sorted Lists.cpp
+++ Linked list/21_Merge Two Sorted Lists.cpp
@@ -6,46 +6,31 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        //first create a empty pointer used to hold the new combined list
-        ListNode *ptr;
-        if(l1->val >= l2->val){
-            ptr = l2;
-            l2 = l2->next;
-        }
-        else{
-            ptr = l1;
-            l1 = l1->next;
-        }
-        ListNode *dummyhead = ptr;
-        while(l1 != NULL && l2 != NULL){
-            if (l1->val >= l2->val){
-                ptr->next = l2;
-                l2 = l2->next;
-                ptr = ptr->next;
-            }
-            else{
-                ptr->next = l1;
-                l1 = l1->next;
-                ptr = ptr->next;
-            }
-        }
-        //look for which list finished first
-        if (l1 == NULL){
-            //append all the rest of l2 to ptr
-            ptr->next = l2;
-        }
-        else{
-            ptr->next = l1;
-        }
-        return dummyhead;
-    }
-};
-
+ class Solution {
+ public:
+     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+         if(l1 == NULL) return l2;
+         if(l2 == NULL) return l1;
+         //first create a empty pointer used to hold the new combined list
+         ListNode *ptr;
+         ListNode *dummyhead = ptr;
+         while(l1 != NULL && l2 != NULL){
+             if (l1->val >= l2->val){
+                 ptr->next = l2;
+                 l2 = l2->next;
+                 ptr = ptr->next;
+             }
+             else{
+                 ptr->next = l1;
+                 l1 = l1->next;
+                 ptr = ptr->next;
+             }
+         }
+         //look for which list finished first
+         ptr->next = l1 ? l1:l2;
+         return dummyhead->next;
+     }
+ };
 
 
 
