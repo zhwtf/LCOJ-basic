ecd899fff7380306b34ff6b3b3eca0f184cb58f9
haozheng <zhwtf123@gmail.com>
2017-01-03 00:10:05 -0800

    03-00

---
+++ Medium_level/22_Generate_parentheses.cpp
@@ -0,0 +1,26 @@
+//recurive solution
+//The idea is intuitive. Use two integers to count the remaining left parenthesis (n) and the right parenthesis (m) to be added.
+//At each function call add a left parenthesis if n >0 and add a right parenthesis if m>0. Append the result and terminate recursive calls when both m and n are zero.
+
+class Solution{
+public:
+  vector<string> generateParenthesis(int n){
+    vector<string> result;
+    recur(result, "", n, 0);
+    return result;
+
+  }
+
+  void recur(vector<string> &v, string str, int n, int m) {
+    if (n==0 && m==0){
+      v.push_back(str);
+      return;
+    }
+    if (m > 0) {
+      recur(v, str + ")", n, m-1);
+    }
+    if (n > 0) {
+      recur(v, str + "(", n-1, m+1);
+    }
+  }
+};
