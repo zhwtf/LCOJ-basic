a9d7ac2c6d51fa48f9b698236f074a84fc94cbbd
haozheng <zhwtf123@gmail.com>
2017-01-06 16:43:02 -0800

    06-01

---
+++ Easy_level/463_Island_perimeter.cpp
@@ -0,0 +1,19 @@
+//1.find how many 1 in the map. If without the consideration of surrounding cells, the total perimeter should be the total amount of 1 times 4.
+//2.find how many cell walls that connect with both lands. We need to deduct twice of those lines from total perimeter
+
+int islandPerimeter(vector<vector<int>>& grid) {
+        int count=0, repeat=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0; j<grid[i].size();j++)
+                {
+                    if(grid[i][j]==1)
+                    {
+                        count ++;
+                        if(i!=0 && grid[i-1][j] == 1) repeat++;
+                        if(j!=0 && grid[i][j-1] == 1) repeat++;
+                    }
+                }
+        }
+        return 4*count-repeat*2;
+    }
