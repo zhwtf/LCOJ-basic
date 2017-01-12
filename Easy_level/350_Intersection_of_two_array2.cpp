3a793bc251cd32a09d426ba84e4cd712059f5895
haozheng <zhwtf123@gmail.com>
2017-01-11 23:16:03 -0800

    11-01

--- Easy_level/350_Intersection_of_two_array2.cpp
+++ Easy_level/350_Intersection_of_two_array2.cpp
@@ -1,3 +1,4 @@
+//Given two arrays, write a function to compute their intersection.
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -12,3 +13,15 @@ public:
         return result;
     }
 };
+
+
+vector<int> intersect(vector<int>& a, vector<int>& b) {
+    unordered_map<int, int> ctr;
+    for (int i : a)
+        ctr[i]++;
+    vector<int> out;
+    for (int i : b)
+        if (ctr[i]-- > 0)
+            out.push_back(i);
+    return out;
+}
