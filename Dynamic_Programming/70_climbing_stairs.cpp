9ae99e9b3692c8fcc144219f266c5f1bd2be76e3
haozheng <zhwtf123@gmail.com>
2017-01-12 17:49:29 -0800

    12-03

---
+++ Dynamic_Programming/70_climbing_stairs.cpp
@@ -0,0 +1,21 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+'''
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0 || n == 1 || n == 2){return n;}
+        int *mem = new int[n];
+        mem[0] = 1;
+        mem[1] = 2;
+        for(int i = 2; i < n; i++){
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+        return mem[n-1];
+    }
+};
