5144e16f1a3f05c94f89d809a7f57d83df204d6f
haozheng <zhwtf123@gmail.com>
2017-01-17 22:15:49 -0800

    17-02

---
+++ binary tree/437_Path_Sum3.cpp
@@ -0,0 +1,32 @@
+'''
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+'''
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        return sumUp(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+    }
+private:
+    int sumUp(TreeNode* root, int pre, int& sum){
+        if(!root) return 0;
+        int current = pre + root->val;
+        return (current == sum) + sumUp(root->left, current, sum) + sumUp(root->right, current, sum);
+    }
+};
